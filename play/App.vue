7ebb6fa9a753d91c09ef14deadd112cdabb2dc08
zhepei.shen <zhepei.shen@eslink.com>
2022-05-05 10:11:31 +0800

    feat: 🎸 init

---
+++ play/App.vue
@@ -0,0 +1,24 @@
+<template lang="pug"></template>
+<script lang="ts">
+import { on, off } from '../packages'
+export default {
+	name: 'play',
+	data() {
+		return {}
+	},
+	methods: {
+		selfEvent(e) {
+			console.log(e)
+			console.log(1)
+		},
+	},
+	mounted() {
+		on(window, 'click', this.selfEvent)
+	},
+}
+</script>
+<style lang="scss" scoped>
+.play-box {
+	margin: 80px;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,52 @@
+const pkg = require('./package.json')
+const isProduction = process.env.NODE_ENV === 'production'
+
+module.exports = {
+	pages: {
+		index: {
+			entry: './play/main.ts',
+		},
+	},
+	assetsDir: './',
+	devServer: {
+		port: 3000,
+		hot: true,
+		open: true,
+	},
+	css: {
+		extract: process.env.VUE_APP_BUILD_MODE === 'NPM',
+		sourceMap: false,
+	},
+	configureWebpack: config => {
+		config.resolve.extensions = ['.js', '.vue', '.json', '.ts', '.tsx']
+		if (process.env.VUE_APP_BUILD_MODE === 'NPM') {
+			config.externals = [
+				{
+					vue: {
+						root: 'Vue',
+						commonjs: 'vue',
+						commonjs2: 'vue',
+						amd: 'vue',
+					},
+				},
+			]
+		} else {
+			config.externals = [
+				{
+					vue: 'Vue',
+				},
+			]
+		}
+	},
+	chainWebpack: config => {
+		config.plugin('define').tap(args => {
+			args[0]['process.env'].version = JSON.stringify(pkg.version)
+			return args
+		})
+		if (isProduction) {
+			config.plugins.delete('prefetch')
+		} else {
+			config.resolve.symlinks(true)
+		}
+	},
+}
