7fe7ebe6401a2cb81ece6415a5750ce5b2571614
zhepei.shen <zhepei.shen@eslink.com>
2022-05-05 16:12:33 +0800

    feat: 🎸 添加window变量

--- packages/index.ts
+++ packages/index.ts
@@ -1,1 +1,15 @@
-export * from './dom'
+import { on, off } from './dom'
+
+const version = process.env.version
+const util = {
+	version,
+	on,
+	off,
+}
+if (window !== undefined) {
+	if (!window['cakeV-util']) {
+		window['cakeV-util'] = util
+	}
+}
+export { version, on, off }
+export default util
